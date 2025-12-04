78960b060193f6ccf9fcf14493672fb248bddf3d
Ashley <ashleykdwyer@gmail.com>
2025-12-03 22:57:07 -0330

    Updated code for reading and writing money file

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
 def read_money():
     money = []
-    with open("money.txt") as file:
-        for line in file:
-           print("Money: ", line)
+    with open("money.txt", "r") as file:
+        money = float(file.read())
+        print(f"Money:{money}")
+    return money
 
-def write_money():
+def write_money(money):
     with open("money.txt", "w") as file:
         file.write
