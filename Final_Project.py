3dcb0a90ca92f36589f747d617858b2748dd92a8
Ashley <ashleykdwyer@gmail.com>
2025-12-03 22:53:47 -0330

    Added code to display winner and money balance

--- Final_Project.py
+++ Final_Project.py
@@ -23,9 +23,7 @@ def hands(deck):
     return dealer_hand, player_hand
 
 
-
-
-def play_game(deck, dealer_hand, player_hand):
+def play_game(deck, dealer_hand, player_hand, money):
     bet = float(input("Bet amount: "))
     print()
     print("DEALER'S SHOW CARD:")
@@ -44,28 +42,55 @@ def play_game(deck, dealer_hand, player_hand):
             for card in player_hand:
                 print(f"{card[1]} of {card[0]}")
         elif hit_stand== 'stand':
+            dealer_hand = dealer_totals(deck, dealer_hand)
             print("\nDEALER'S CARDS:")
             for card in dealer_hand:
                 print(f"{card[1]} of {card[0]}")
             break
-    player_total = 0
-    for card in player_hand:
-        player_total += card[2]
+    player_total = totals(player_hand)
+    dealer_total = totals(dealer_hand)
     print(f"\nYOUR POINTS:\t",player_total)
-    dealer_total = 0
-    for card in dealer_hand:
-        dealer_total += card[2]
     print(f"DEALER'S POINTS:",dealer_total)
-        
-  
+    if player_total > 21:
+        print("\nPlayer Bust!")
+        money -= bet
+    elif dealer_total > 21:
+        print("\nDealer Bust! You Win!")
+        money = money - bet + bet *1.5
+    elif player_total > dealer_total:
+        print("\nYou Win!!")
+        money = money - bet + bet *1.5
+    elif player_total < dealer_total:
+        print("\nSorry. You lose.")
+        money -= bet
+    else:
+        print("It is a tie!")
+    print(f"Money: ${money}")
+    return money
+
+def totals(hand):
+    total = sum(card[2] for card in hand)
+    aces = sum(1 for card in hand if card[1] == 'A')
+    while total > 21 and aces:
+        total -= 10
+        aces -=1
+    return total
+
+def dealer_totals(deck, dealer_hand):
+    while totals(dealer_hand) < 17:
+        dealer_hand.append(deck.pop())
+    return dealer_hand
+
+
 
 def main():
     print("BLACKJACK!")
     print("Blackjack payout is 3:2\n")
-    deck = deck_cards()
     money = db.read_money()
+    deck = deck_cards()
     dealer_hand, player_hand = hands(deck)
-    play_game(deck, dealer_hand, player_hand)
+    money = play_game(deck, dealer_hand, player_hand, money)
+    db.write_money(money)
    
 
 
