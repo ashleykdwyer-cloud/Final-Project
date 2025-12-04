66485491c22f5bdb9e382b850d32c6adcee4b6ec
Ashley <ashleykdwyer@gmail.com>
2025-12-04 09:40:58 -0330

    Updated code to check if player or dealer has blackjack after 2 cards each are delt. insert code to check if player bust over 21 before dealers second card is shown.

--- Final_Project.py
+++ Final_Project.py
@@ -1,5 +1,4 @@
 import random
-import csv
 import db
 
 
@@ -22,6 +21,10 @@ def hands(deck):
     player_hand = hands[1]
     return dealer_hand, player_hand
 
+def check_blackjack(hand):
+    return len(hand) == 2 and totals(hand) == 21
+         
+
 
 def play_game(deck, dealer_hand, player_hand, money):
     bet = float(input("Bet amount: "))
@@ -32,6 +35,30 @@ def play_game(deck, dealer_hand, player_hand, money):
     print("YOUR CARDS:")
     for card in player_hand:
         print(f"{card[1]} of {card[0]}")
+
+        
+    player_blackjack = check_blackjack(player_hand)
+    dealer_blackjack = check_blackjack(dealer_hand)
+    if player_blackjack or dealer_blackjack:
+        print("\nDEALER'S CARDS:")
+        for card in dealer_hand:
+            print(f"{card[1]} of {card[0]}")
+
+            if player_blackjack:
+                print("\n BLACKJACK! You win!!")
+                money += bet * 1.5
+                print(f"Money: ${money}")
+                return money
+            elif dealer_blackjack:
+                print("\n Dealer has blackjack, you lose!")
+                money -= bet
+                print(f"Money: ${money}")
+                return money
+            elif player_blackjack or dealer_blackjack:
+                print("You both have blackjack, it is a tie!")
+                print(f"Money: ${money}")
+
+    
     while True:
         print()
         hit_stand = input("Hit or stand? (hit/stand): ").lower()
@@ -41,12 +68,17 @@ def play_game(deck, dealer_hand, player_hand, money):
             print("\nYOUR CARDS:")
             for card in player_hand:
                 print(f"{card[1]} of {card[0]}")
+            if totals(player_hand) > 21:
+                break
         elif hit_stand== 'stand':
-            dealer_hand = dealer_totals(deck, dealer_hand)
-            print("\nDEALER'S CARDS:")
-            for card in dealer_hand:
-                print(f"{card[1]} of {card[0]}")
             break
+        
+    dealer_hand = dealer_totals(deck, dealer_hand)
+    print("\nDEALER'S CARDS:")
+    for card in dealer_hand:
+        print(f"{card[1]} of {card[0]}")
+    
+    
     player_total = totals(player_hand)
     dealer_total = totals(dealer_hand)
     print(f"\nYOUR POINTS:\t",player_total)
@@ -56,10 +88,10 @@ def play_game(deck, dealer_hand, player_hand, money):
         money -= bet
     elif dealer_total > 21:
         print("\nDealer Bust! You Win!")
-        money = money - bet + bet *1.5
+        money += bet
     elif player_total > dealer_total:
         print("\nYou Win!!")
-        money = money - bet + bet *1.5
+        money += bet
     elif player_total < dealer_total:
         print("\nSorry. You lose.")
         money -= bet
@@ -87,11 +119,17 @@ def main():
     print("BLACKJACK!")
     print("Blackjack payout is 3:2\n")
     money = db.read_money()
-    deck = deck_cards()
-    dealer_hand, player_hand = hands(deck)
-    money = play_game(deck, dealer_hand, player_hand, money)
-    db.write_money(money)
-   
+    play_again = 'y'
+    while True:
+        deck = deck_cards()
+        dealer_hand, player_hand = hands(deck)
+        money = play_game(deck, dealer_hand, player_hand, money)
+        db.write_money(money)
+        play_again = input("\nPlay again? (y/n): ")
+        if play_again != 'y':
+            break
+    print("Come back soon!\nBye!")
+       
 
 
 if __name__ =="__main__":
